bbc59e0a474ad97a68ae506f1d06eba948ecf618
Sebastien Denommee <mrsebastino@gmail.com>
2020-08-10 10:30:05 +0000

    customised admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+
     region = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
@@ -9,6 +12,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL
     )
@@ -19,6 +23,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     rating = models.IntegerField(default=0, null=True, blank=True)
+    is_exclusive = models.BooleanField(default=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
