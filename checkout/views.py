d1ea3fa4b62b87d06f9f6f768184ad5bc087c563
Sebastien Denommee <mrsebastino@gmail.com>
2020-08-21 15:53:25 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H59X8BoP8OfuZRQrL2MMfCpNcfwRPs2oEYimPKpnkP6SekWGC1zV3i1IkGAOXRPfn6I4BbvHBhBJk0UZrql0sPP002O0vPffe',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
