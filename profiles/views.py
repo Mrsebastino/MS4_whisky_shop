6848361be41d530267189935bf1eff035aa8b0e6
Sebastien Denommee <mrsebastino@gmail.com>
2020-08-27 13:31:24 +0000

    added profile app

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def profile(request):
+    """ Display user profiles """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
