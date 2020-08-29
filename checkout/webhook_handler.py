ff4b102ec859818569af4428f1e0e38a14e60a27
Sebastien Denommee <mrsebastino@gmail.com>
2020-08-29 11:32:49 +0000

    added confirmation email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -26,7 +26,8 @@ class StripeWH_Handler:
 
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            {'order': order,
+             'contact_email': settings.DEFAULT_FROM_EMAIL})
 
         send_mail(
             subject,
@@ -144,7 +145,7 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        self._send_confirmation_email(order)
+        self._send_confirmation_email(order, product)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS:'
                      'Created order in webhook'), status=200)
