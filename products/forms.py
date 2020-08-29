532d0111c3143665bba12d9b8d7b7df7ea55dafa
Sebastien Denommee <mrsebastino@gmail.com>
2020-08-29 13:12:49 +0000

    added add product functionality for shop owner

---
+++ products/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Product, Category, Special
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        specials = Special.objects.all()
+        friendly_special_names = [
+            (s.id, s.get_friendly_name()) for s in specials]
+
+        self.fields['category'].choices = friendly_names
+        self.fields['special'].choices = friendly_special_names
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
