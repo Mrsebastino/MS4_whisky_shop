d0605ef9082182d4477c62abc03e25fe0f15ef81
Sebastien Denommee <mrsebastino@gmail.com>
2020-09-09 12:11:06 +0000

    added  ordering by reverse date in  for order history

--- checkout/models.py
+++ checkout/models.py
@@ -39,6 +39,9 @@ class Order(models.Model):
         max_length=254, null=False,
         blank=False, default='')
 
+    class Meta:
+        ordering = ['-date']
+
     def _generate_order_number(self):
         """
         Generate unique order number
