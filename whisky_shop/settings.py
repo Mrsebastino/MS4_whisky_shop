53b64dea9a695cfcb7cc1e6026fffd1cda773301
Sebastien Denommee <mrsebastino@gmail.com>
2022-11-19 11:04:04 +0000

    revert last commit

--- whisky_shop/settings.py
+++ whisky_shop/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'whisky_shop.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('RENDER_EXTERNAL_HOSTNAME'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
