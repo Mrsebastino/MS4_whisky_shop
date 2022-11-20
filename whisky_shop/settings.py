d034811250e3e0fd537bef9684d087670363c01b
Sebastien Denommee <mrsebastino@gmail.com>
2022-11-20 15:34:08 +0000

    test unique true

--- whisky_shop/settings.py
+++ whisky_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['whisky-shop-uisge-beatha.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['', 'localhost']
 
 # Add Render.com URL to allowed hosts
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
