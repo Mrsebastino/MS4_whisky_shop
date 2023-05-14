d23ca012070a8a59746c13e969298f309554f995
Sebastien Denommee <mrsebastino@gmail.com>
2023-05-14 13:06:13 +0000

    test

--- whisky_shop/settings.py
+++ whisky_shop/settings.py
@@ -33,9 +33,9 @@ ALLOWED_HOSTS = ['whisky-shop-uisge-beatha.onrender.com', 'localhost']
 
 
 # Add Render.com URL to allowed hosts
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
